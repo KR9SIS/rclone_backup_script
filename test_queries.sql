cd699433aca3b14f14bb75864de045d96e05416f
kr9sis <kristoferhelgi@protonmail.com>
2024-07-25 21:48:01 +0200

    Mostly cleaned up test_queries.sql after installing linter

--- test_queries.sql
+++ test_queries.sql
@@ -1,47 +1,52 @@
 -- ALTER SEQUENCE times_id_seq RESTART WITH 1;
-DROP SEQUENCE BackupNum;
-DROP TABLE IF EXISTS Times;
-DROP TABLE IF EXISTS Folders;
+DROP SEQUENCE BACKUPNUM;
+DROP TABLE IF EXISTS TIMES;
+DROP TABLE IF EXISTS FOLDERS;
 
-CREATE TABLE Folders(
-    folder_path VARCHAR(255) PRIMARY KEY
+CREATE TABLE FOLDERS (
+    FOLDER_PATH VARCHAR(255) PRIMARY KEY
 );
 
-CREATE TABLE Times(
-    parent_path VARCHAR(255),
-    file_path VARCHAR(255) PRIMARY KEY,
-    modification_time CHAR(16) NOT NULL,
-    FOREIGN KEY (parent_path) REFERENCES Folders(folder_path)
+CREATE TABLE TIMES (
+    PARENT_PATH VARCHAR(255),
+    FILE_PATH VARCHAR(255) PRIMARY KEY,
+    MODIFICATION_TIME CHAR(16) NOT NULL,
+    FOREIGN KEY (PARENT_PATH) REFERENCES FOLDERS (FOLDER_PATH)
 
 );
 
-CREATE SEQUENCE BackupNum START 1;
+CREATE SEQUENCE BACKUPNUM START 1;
 
-INSERT INTO Folders(folder_path) VALUES('/parent/');
-INSERT INTO Folders(folder_path) VALUES('/parent/child/');
+select *
+from times
+where modification_time = '0000-00-00 00:00'
+;
 
-INSERT INTO Times(folder_path, file_path, modification_time) VALUES('/parent/', 'parent/child/', '2024-06-17 13:32');
-INSERT INTO Times(folder_path, file_path, modification_time) VALUES('/parent/child/', '/parent/child/1', '2024-06-17 13:32');
-INSERT INTO Times(folder_path, file_path, modification_time) VALUES('/parent/child/', '/parent/child/2', '2024-06-17 13:32');
-INSERT INTO Times(folder_path, file_path, modification_time) VALUES('/parent/child/', '/parent/child/3', '2024-06-17 13:32');
+select *
+from times
+where folder_path = '/home/kr9sis/PDrive/Barnabókinn mín/'
 
-SELECT * FROM Folders
-SELECT * FROM Times WHERE Times.folder_path = '/parent/child/'
+select *
+from times
+where
+    parent_path
+    = '/home/kr9sis/PDrive/School/MK/2019/Haust 2019/ALÞV2BA05/Viðfangsefni 8/'
+
+select *
+from folders
+;
+select *
+from times
+limit 30
+;
+
+select *
+from times
+where parent_path = '/home/kr9sis/PDrive/Code/Py/rclone_backup_script/Older Versions/'
+;
+
+select distinct parent_path
+from times
+;
 
 
-SELECT * FROM Times
-WHERE modification_time = '0000-00-00 00:00';
-
-SELECT * FROM Times
-WHERE folder_path = '/home/kr9sis/PDrive/Barnabókinn mín/'
-
-SELECT * FROM Times
-WHERE parent_path = '/home/kr9sis/PDrive/School/MK/2019/Haust 2019/ALÞV2BA05/Viðfangsefni 8/'
-
-SELECT * FROM Folders;
-SELECT * FROM Times LIMIT 30;
-
-SELECT * FROM Times
-WHERE parent_path = '/home/kr9sis/PDrive/Code/Py/rclone_backup_script/Older Versions/';
-
-SELECT DISTINCT parent_path FROM Times;
