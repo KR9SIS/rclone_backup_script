efeb2780febe090dc968b2140b66c4f7450a12e2
kr9sis <kristoferhelgi@protonmail.com>
2024-10-15 21:06:51 +0000

    Ported over dir ops from v1 main.py

---
+++ src_v2/dir_ops.py
@@ -0,0 +1,176 @@
+"""
+Class to recursively go through every file in a directory and all it's subdirectories
+extracting their modification times.
+"""
+
+from pathlib import Path
+from subprocess import CalledProcessError, TimeoutExpired, run
+from textwrap import dedent
+
+
+def get_files_in_cwd(self, cwd: Path) -> list[tuple[str]]:
+    """
+    Function to get all files within the current working directory
+    and their modification time and return a str containing that
+    information sorted with most recent modification first
+    """
+
+    with open(self.backup_log, "a", encoding="utf-8") as log_file:
+        ret = []
+        for file in cwd.iterdir():
+            if file.name.startswith(".") or file.is_symlink():
+                continue  # Get rid of all dotfiles and symlinks
+
+            stat_cmd = ["stat", "-c", "%n %y", str(file)]
+            try:
+                stat_out = run(stat_cmd, check=True, timeout=10, capture_output=True)
+                stat_out = stat_out.stdout.decode("utf-8")
+
+            except CalledProcessError as e:
+                print(
+                    f"Error occured with stat command\nCWD:\n{cwd}\nCMD:\n{stat_cmd}\nError:\n{e}",
+                    file=log_file,
+                )
+                stat_out = ""
+
+            except TimeoutExpired as e:
+                print(
+                    f"Error occured with stat command\nCWD:\n{cwd}\nCMD:\n{stat_cmd}\nError:\n{e}",
+                    file=log_file,
+                )
+                stat_out = ""
+
+            if stat_out:
+                mod_time = stat_out[-36:-7]
+                filename = stat_out[:-37]
+                ret.append((filename, mod_time))
+
+    return ret
+
+
+def create_db_files_dict(self, cwd: Path) -> dict[Path, str]:
+    """
+    Gets the files from the DB and turns them into a dictionary
+    where the file paths are keys and mod times are values
+    """
+    db_files = self.conn.execute(
+        """
+            SELECT file_path, modification_time
+            FROM Times
+            WHERE parent_path = ?;
+            """,
+        (str(cwd),),
+    ).fetchall()
+
+    return {Path(db_f_tup[0]): db_f_tup[1] for db_f_tup in db_files}
+
+
+def add_or_del_from_db(self, files, db_files):
+    """
+    Clean up difference between local directory and database
+    """
+    local_files: set[Path] = set(files)
+    cloud_files: set[Path] = set(db_files)
+
+    diff = local_files.symmetric_difference(cloud_files)
+
+    for file in diff:
+        if file not in cloud_files:  # File was created locally
+            parent_dir = file.parent
+            if file.is_dir():
+                self.conn.execute(
+                    """
+                        INSERT INTO Folders(folder_path)
+                        VALUES(?);
+                        """,
+                    (str(file),),
+                )
+                self.conn.execute(
+                    """
+                        INSERT INTO Times(parent_path, file_path, modification_time)
+                        VALUES(?,?,?);
+                        """,
+                    (str(parent_dir), str(file), files[file]),
+                )
+            else:
+                self.conn.execute(
+                    """
+                        INSERT INTO Times(parent_path, file_path, modification_time)
+                        VALUES(?,?,?);
+                        """,
+                    (str(parent_dir), str(file), files[file]),
+                )
+            self.modified[file] = files[file]
+            # Make sure mod time is different so the file will be synced
+            db_files[file] = "0000-00-00 00:00"
+
+        elif file not in local_files:  # File was deleted locally
+            self.conn.execute(
+                """
+                    DELETE FROM Times 
+                    WHERE parent_path=?;
+                    """,
+                (str(file),),
+            )
+
+            self.conn.execute(
+                """
+                    DELETE FROM Folders
+                    WHERE folder_path=?;
+                    """,
+                (str(file),),
+            )
+            self.conn.execute(
+                """
+                    DELETE FROM Times
+                    WHERE file_path=?;
+                    """,
+                (str(file),),
+            )
+
+            self.modified[file] = db_files[file]
+
+            # Make sure mod time is different so the file will be synced
+            files[file] = "0000-00-00 00:00"
+
+    self.conn.commit()
+
+
+def check_if_modified(self, files: dict[Path, str], db_files: dict[Path, str]):
+    """
+    Check the given files and see if they have been
+    modified or not if they have been then either check its
+    subdirectories or log the file as modified
+    """
+
+    for file, modification_time in files.items():
+        if file.is_dir():
+            self.get_modified_files(file)
+
+        if file.is_file() and modification_time != db_files[file]:
+            self.modified[file] = modification_time
+        self.cur_file += 1
+
+    self.conn.commit()
+
+
+def get_modified_files(self, cwd: Path):
+    """
+    Recursively checks the cwd and every subdirectory within it
+    """
+    if self.file_count != -99999:
+        percent = round((self.cur_file / self.file_count) * 100)
+        print(f"{percent}%", end=" ")
+    print(f"In {cwd}")
+
+    files = self.get_files_in_cwd(cwd)
+    if not files:
+        return
+
+    files = {Path(f_tup[0]): f_tup[1] for f_tup in files}
+    db_files = self.create_db_files_dict(cwd)
+
+    if len(files) != len(db_files) or files != db_files:
+        self.add_or_del_from_db(files, db_files)
+
+    self.check_if_modified(files, db_files)
--- src_v2/main_v2.py
+++ src_v2/main_v2.py
@@ -10,9 +10,9 @@
 from contextlib import closing
 from pathlib import Path
 from sqlite3 import connect
-from subprocess import CalledProcessError, TimeoutExpired, run
 
 from db_ops import get_count_or_setup_db
+from dir_ops import get_modified_files
 from rclone_ops import rclone_check_connection
 
 
@@ -36,67 +36,4 @@ class RCloneBackupScript:
         if rclone_check_connection(self, REMOTE_DIRECTORY):
             with closing(connect(db_file)) as self.db_conn:
                 get_count_or_setup_db(self, LOCAL_DIRECTORY)
-                self.get_modified_files(cwd=Path(LOCAL_DIRECTORY))
-
-    def get_files_in_cwd(self, cwd: Path) -> list[str]:
-        """
-        Function to get all files within the current working directory
-        and their modification time and return a str containing that
-        information sorted with most recent modification first
-        """
-        # TODO: FINISH GOING OVER THIS FUNCTION BEFORE PORTING OVER OTHER FUNCTIONS
-        # TODO: CHECK OUT dedent FROM Minni_Vinna
-
-        with open(self.backup_log, "a", encoding="utf-8") as log_file:
-            ret = []
-            for file in cwd.iterdir():
-                if file.name.startswith(".") or file.is_symlink():
-                    continue  # Get rid of all dotfiles
-
-                stat_cmd = ["stat", "-c", "%n %y", str(file)]
-                try:
-                    stat_out = run(
-                        stat_cmd, check=True, timeout=10, capture_output=True
-                    )
-                    stat_out = stat_out.stdout.decode("utf-8")
-
-                except CalledProcessError as e:
-                    print(
-                        f"CWD:\n{cwd}\nError occured with getting files command\nError:\n{e}",
-                        file=log_file,
-                    )
-                    stat_out = ""
-
-                except TimeoutExpired as e:
-                    print(
-                        f"CWD:\n{cwd}\nError occured with getting files command\nError:\n{e}",
-                        file=log_file,
-                    )
-                    stat_out = ""
-
-                mod_time = stat_out[-36:-7]
-                filename = stat_out[:-37]
-                ret.append((filename, mod_time))
-
-        return ret
-
-    def get_modified_files(self, cwd: Path):
-        """
-        Recursively checks the cwd and every subdirectory within it
-        """
-        if self.file_count != -99999:
-            percent = round((self.cur_file / self.file_count) * 100)
-            print(f"{percent}%", end=" ")
-        print(f"In {cwd}")
-
-        files = self.get_files_in_cwd(cwd)
-        if not files:
-            return
-
-        files = {Path(file[0]): file[1] for file in files}
-        db_files = self.create_db_files_dict(cwd)
-
-        if len(files) != len(db_files) or files != db_files:
-            self.add_or_del_from_db(files, db_files)
-
-        self.check_if_modified(files, db_files)
+                get_modified_files(self, cwd=Path(LOCAL_DIRECTORY))
