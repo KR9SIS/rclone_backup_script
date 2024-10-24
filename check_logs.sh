75c50a723d9e9b229255c91d6a900aba90e87371
kr9sis <kristoferhelgi@protonmail.com>
2024-10-24 19:36:37 +0000

    Updated script to check logs
    I added functionality to specifically search for unsynced files and
    adding headers to explain data

--- check_logs.sh
+++ check_logs.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+echo "All logged files:"
+echo "date|file_path|synced"
 sqlite3 src/RCloneBackupScript.db <<EOF
 SELECT * FROM Log;
 EOF
-
+echo "\nFiles which did not sync:"
+echo "date|file_path|synced"
+sqlite3 src/RCloneBackupScript.db <<EOF
+SELECT * FROM Log WHERE synced = 0;
+EOF
