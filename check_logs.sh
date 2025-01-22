88311d9827a4900e86415dc828cbd2bc1d490deb
kr9sis <kristoferhelgi@protonmail.com>
2025-01-22 22:21:15 +0000

    Added counter for number of files which haven't synced

--- check_logs.sh
+++ check_logs.sh
@@ -4,10 +4,25 @@ echo "date|file_path|synced"
 sqlite3 src/RCloneBackupScript.db <<EOF
 SELECT * FROM Log;
 EOF
+
 echo "\nFiles which did not sync:"
 echo "date|file_path|synced"
 sqlite3 src/RCloneBackupScript.db <<EOF
 SELECT *
+FROM Log AS l1
+WHERE l1.synced = 0
+AND NOT EXISTS (
+    SELECT 1
+    FROM Log AS l2
+    WHERE l2.file_path = l1.file_path
+    AND l2.synced = 1
+    AND l2.date > l1.date
+);
+EOF
+
+echo "\nCount of files which did not sync:"
+sqlite3 src/RCloneBackupScript.db <<EOF
+SELECT COUNT(*)
 FROM Log AS l1
 WHERE l1.synced = 0
 AND NOT EXISTS (
